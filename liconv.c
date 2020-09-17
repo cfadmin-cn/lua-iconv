d25f0ee09ba89e6117c053c7535a9e9ff5aa7150
Mr.che <869646063@qq.com>
2020-09-18 01:00:54 +0800

    Update liconv.c
    更新代码可读性

--- liconv.c
+++ liconv.c
@@ -16,17 +16,41 @@
   #define _LIBICONV_VERSION (1 << 8)
 #endif
 
-/* 获取libiconv 大、小版本号 */
-#define luaL_geticonv(max, min) ({ if (max) *max = _LIBICONV_VERSION >> 8; if (min) *min = (_LIBICONV_VERSION - ((_LIBICONV_VERSION >> 8) << 8));})
+#define CONVERT_TO (0)
+#define CONVERT_FROM (1)
 
 /* 为lib注入 key -> number 数据 */
-#define luaL_setkn(L, k, n) ({ lua_pushstring(L, k); lua_pushnumber(L, n) ; lua_rawset(L, -3);})
+#define luaL_setkn(L, k, n) ({ lua_pushstring(L, k); lua_pushnumber(L, n) ; lua_rawset(L, -3); })
 
 /* 为lib注入 key -> string 数据 */
-#define luaL_setkv(L, k, v) ({ lua_pushstring(L, k); lua_pushstring(L, v) ; lua_rawset(L, -3);})
+#define luaL_setkv(L, k, v) ({ lua_pushstring(L, k); lua_pushstring(L, v) ; lua_rawset(L, -3); })
+
+/* 获取libiconv 大、小版本号 */
+static inline void luaL_geticonv(lua_Integer* max, lua_Integer* min) {
+  if (max)
+    *max = _LIBICONV_VERSION >> 8;
+  if (min)
+    *min = (_LIBICONV_VERSION - ((_LIBICONV_VERSION >> 8) << 8));
+}
+
+/* 增加自动判断 */ 
+static inline iconv_t new_iconv(uint8_t mode, const char* opcode) {
+  if (mode == CONVERT_FROM) 
+    return iconv_open("UTF-8", opcode);
+
+  if (mode == CONVERT_TO)
+    return iconv_open(opcode, "UTF-8");
+
+  return (iconv_t)-1;
+}
 
 // 开始转换
-static inline int convert(lua_State *L, iconv_t cd, const char* text, size_t size) {
+static inline int convert(lua_State *L, uint8_t mode, const char* opcode, const char* text, size_t size) {
+
+  iconv_t cd = new_iconv(mode, opcode);
+  if (cd == (iconv_t)-1)
+    return luaL_error(L, "Unsupported character set: %s", opcode);
+
   luaL_Buffer B;
   size_t outsize = size * 3;
   char *out_buf = luaL_buffinitsize(L, &B, outsize);
@@ -57,11 +81,7 @@ static int lconvert_from(lua_State *L) {
   if (!text)
     return luaL_error(L, "Please fill in the text content to be converted.");
 
-  iconv_t cd = iconv_open("UTF-8", opcode);
-  if (cd == (iconv_t)-1)
-    return luaL_error(L, "Unsupported character set: %s", opcode);
-
-  return convert(L, cd, text, size);
+  return convert(L, CONVERT_FROM, opcode, text, size);
 }
 
 // 从UTF-8转换为其他字符集
@@ -76,11 +96,7 @@ static int lconvert_to(lua_State *L) {
   if (!text)
     return luaL_error(L, "Please fill in the text content to be converted.");
 
-  iconv_t cd = iconv_open(opcode, "UTF-8");
-  if (cd == (iconv_t)-1)
-    return luaL_error(L, "Unsupported character set: %s", opcode);
-
-  return convert(L, cd, text, size);
+  return convert(L, CONVERT_TO, opcode, text, size);
 }
 
 //  初始化内置库
@@ -88,10 +104,9 @@ static inline void luaL_add_iconv_version(lua_State *L) {
   /* 根据情况拿到大、小版本号 */
   lua_Integer max = 0; lua_Integer min = 0;
   luaL_geticonv(&max, &min);
-
-  // 导入libiconv版本号
-  luaL_setkn(L, "LIBICONV_VERSION", max + /* 如果小版本大于100 */ (min > 99 ? min * 1e-3 : min * 1e-2 ));
-  // 导入lua-iconv版本号
+  /* 导入libiconv版本号 */
+  luaL_setkn(L, "LIBICONV_VERSION", max + /* 如果小版本是三位数 */ ( min > 99 ? min * 1e-3 : min * 1e-2 ));
+  /* 导入lua-iconv版本号 */
   luaL_setkn(L, "VERSION", 0.1);
 }
 
