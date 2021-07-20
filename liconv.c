a9a231884bf3e7e687384d9e6de910fa96f8b0b1
Mr.che <869646063@qq.com>
2021-07-20 10:00:03 +0800

    Update liconv.c

--- liconv.c
+++ liconv.c
@@ -24,24 +24,8 @@ static inline void luaL_geticonv(lua_Integer* max, lua_Integer* min) {
     *min = (_LIBICONV_VERSION - ((_LIBICONV_VERSION >> 8) << 8));
 }
 
-/* 增加自动判断 */ 
-static inline iconv_t new_iconv(uint8_t mode, const char* opcode) {
-  if (mode == CONVERT_FROM) 
-    return iconv_open("UTF-8", opcode);
-
-  if (mode == CONVERT_TO)
-    return iconv_open(opcode, "UTF-8");
-
-  return (iconv_t)-1;
-}
-
-// 开始转换
-static inline int convert(lua_State *L, uint8_t mode, const char* opcode, const char* text, size_t size) {
-
-  iconv_t cd = new_iconv(mode, opcode);
-  if (cd == (iconv_t)-1)
-    return luaL_error(L, "[ICONV ERROR]: Unsupported characterset (%s).", opcode);
-
+/* 实际转换代码 */
+static inline int iconv_convert(lua_State *L, iconv_t cd, const char* text, size_t size) {
   luaL_Buffer B;
   size_t outsize = size * 4;
   char *out_buf = luaL_buffinitsize(L, &B, outsize);
@@ -60,6 +44,47 @@ static inline int convert(lua_State *L, uint8_t mode, const char* opcode, const 
   luaL_pushresultsize(&B, (size * 4) - outsize);
   return 1;
 }
+
+/* 增加自动判断 */ 
+static inline iconv_t new_iconv(uint8_t mode, const char* opcode) {
+  if (mode == CONVERT_FROM) 
+    return iconv_open("UTF-8", opcode);
+
+  if (mode == CONVERT_TO)
+    return iconv_open(opcode, "UTF-8");
+
+  return (iconv_t)-1;
+}
+
+// 开始转换
+static inline int convert(lua_State *L, uint8_t mode, const char* opcode, const char* text, size_t size) {
+  iconv_t cd = new_iconv(mode, opcode);
+  if (cd == (iconv_t)-1)
+    return luaL_error(L, "[ICONV ERROR]: Unsupported characterset (%s).", opcode);
+
+  return iconv_convert(L, cd, text, size);
+}
+
+static int lconvert_convert(lua_State *L) {
+  const char* to = luaL_checkstring(L, 1);
+  if (!to)
+    return luaL_error(L, "[ICONV ERROR]: Invalid conversion character set. #1");
+  const char* from = luaL_checkstring(L, 2);
+  if (!from)
+    return luaL_error(L, "[ICONV ERROR]: Invalid conversion character set. #2");
+
+  size_t size = 0;
+  const char* text = luaL_checklstring(L, 3, &size);
+  if (!text)
+    return luaL_error(L, "[ICONV ERROR]: Please fill in the text content to be converted."); 
+  
+  iconv_t cd = iconv_open(to, from);
+  if (cd == (iconv_t)-1)
+    return luaL_error(L, "[ICONV ERROR]: Unsupported characterset (%s, %s).", to, from);
+  
+  return iconv_convert(L, cd, text, size);
+}
+
 
 // 从其他字符集转换为UTF-8
 static int lconvert_from(lua_State *L) {
@@ -106,6 +131,7 @@ LUAMOD_API int
 luaopen_liconv(lua_State *L) {
   luaL_checkversion(L);
   luaL_Reg iconv_libs[] = {
+    { "convert", lconvert_convert },
     { "from", lconvert_from },
     { "to", lconvert_to },
     {NULL, NULL},
